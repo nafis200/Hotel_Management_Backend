f1a3a7e4fd872f8df6c88daa5055a8e6d970900c
nafis200 <nafisahamed14@gmail.com>
2026-01-23 18:06:10 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import httpStatus from "http-status";
+
+
+import path from "path";
+
+const app: Application = express();
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    Message: "Project start..",
+  });
+});
+
+// app.use("/api", router);
+
+// app.use(globalErrorHandler);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "API NOT FOUND!",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found!",
+    },
+  });
+});
+
+export default app;
---
+++ src/app/config/index.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+export default {
+//   env: process.env.NODE_ENV,
+//   port: process.env.PORT,
+//   NODE_ENV: process.env.NODE_ENV,
+//   GOOGLE_DRIVE_FOLDER_ID:process.env.GOOGLE_DRIVE_FOLDER_ID,
+//   GOOGLE_SERVICE_ACCOUNT_BASE64:process.env.GOOGLE_SERVICE_ACCOUNT_BASE64,
+//   googleDrive: {
+//     folderId: process.env.GOOGLE_DRIVE_FOLDER_ID,
+//     clientId: process.env.CLIENT_ID,
+//     clientSecret: process.env.CLIENT_SECRET,
+//     redirectUri: process.env.REDIRECT_URL,
+//     refreshToken: process.env.REFRESH_TOKEN,
+//   },
+  databaseUrl: process.env.DATABASE_URL,
+};
